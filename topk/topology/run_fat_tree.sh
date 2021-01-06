f340a4ffe8b7cf34897e12982101c3e1db49bb79
harry0475 <gomahaera0475@gmail.com>
2021-01-06 15:38:55 +0900

    Path changes

--- topk/topology/run_fat_tree.sh
+++ topk/topology/run_fat_tree.sh
@@ -1,6 +1,6 @@
 ##########################
-BMV2_PATH=~/p4-dev/behavioral-model
-P4C_BM_PATH=~/p4-dev/p4c-bm
+BMV2_PATH=~/p4-projects/behavioral-model
+P4C_BM_PATH=~/p4-projects/p4c-bm
 ##########################
 
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -9,7 +9,7 @@ P4C_BM_SCRIPT=$P4C_BM_PATH/p4c_bm/__main__.py
 SWITCH_PATH=$BMV2_PATH/targets/simple_switch/simple_switch
 CLI_PATH=$BMV2_PATH/targets/simple_switch/sswitch_CLI
 
-sudo PYTHONPATH=$PYTHONPATH:$BMV2_PATH/mininet/ python ~/p4-dev/topk/topology/k_ary_fat_tree_topo.py \
+sudo PYTHONPATH=$PYTHONPATH:$BMV2_PATH/mininet/ python ~/p4-projects/topk/topology/k_ary_fat_tree_topo.py \
     --behavioral-exe $BMV2_PATH/targets/simple_switch/simple_switch \
-    --switch ~/p4-dev/topk/topk_for_daiet.json \
+    --switch ~/p4-projects/topk/topk_for_daiet.json \
     --cli $CLI_PATH
